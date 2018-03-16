e6b74c23f2dfc440073486d8c31c6461dfc1d59d
Student X <simen1127@gmail.com>
2018-03-16 17:29:03 +0800

    ajax CRUD

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,6 @@
 //= require rails-ujs
 //= require turbolinks
 //= require jquery
+//= jquery_ujs 
 //= require bootstrap-sprockets
 //= require_tree .
--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -13,21 +13,19 @@ class TodosController < ApplicationController
     @todo = Todo.new(todo_params)
     @todo.save
 
-    redirect_to todos_path
+
   end
 
   def update
     @todo = Todo.find(params[:id])
     @todo.update_attributes(todo_params)
 
-    redirect_to todos_path
   end
 
   def destroy
     @todo = Todo.find(params[:id])
     @todo.destroy
 
-    redirect_to todos_path
   end
 
   def toggle_check
@@ -36,7 +34,11 @@ class TodosController < ApplicationController
     # 加上驚歎號表示會直接存入資料庫（否則要另外 save)
     # ref: http://api.rubyonrails.org/classes/ActiveRecord/Persistence.html#method-i-toggle
     @todo.toggle!(:done)
-    redirect_to todos_path
+    
+  end
+
+  def edit
+    @todo = Todo.find(params[:id])
   end
 
   private
